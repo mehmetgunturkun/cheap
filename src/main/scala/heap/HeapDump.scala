2aff80e16b2281c093b5db540805538596ca1391
mehmetgunturkun <mehmetcemgunturkun@gmail.com>
2017-01-27 20:46:00 +0300

    Testing different types of records

--- src/main/scala/heap/HeapDump.scala
+++ src/main/scala/heap/HeapDump.scala
@@ -1,6 +1,7 @@
 package heap
 
-import java.io.{DataInputStream, File, FileInputStream}
+import java.io._
+import java.nio.ByteBuffer
 
 import org.joda.time.DateTime
 
@@ -15,11 +16,204 @@ class HeapDump(private val format: String,
                private val startDate: DateTime,
                private val records: DataInputStream) {
 
+  def hasNext():Boolean = records.available() > 0
+
   def nextRecord(): HeapDumpRecord = {
-    val tag: Byte = records.readByte()
-    val ts: Int = records.readInt()
-    val length: Int = records.readInt()
-    StringRecord(tag, length)
+      val tag: Byte = records.readByte()
+      val ts: Int = records.readInt()
+      val length: Int = records.readInt()
+
+
+    tag match {
+      case 0x01 =>
+        val stringId: Long = records.readLong()
+        val contentLength = length - 8
+
+        val arr = Array.ofDim[Byte](contentLength)
+
+
+        records.read(arr)
+        val content = new String(arr)
+
+        StringRecord(stringId, content, length)
+      case 0x02 =>
+
+      case 0x0c =>
+        parseHeapDump(records)
+//        val arr = Array.ofDim[Byte](length)
+//        records.readFully(arr)
+        Record(tag, length)
+      case _ =>
+        val arr = Array.ofDim[Byte](length)
+        records.readFully(arr)
+        Record(tag, length)
+    }
+
+
+  }
+
+  def parseHeapDump(in: DataInputStream): Unit = {
+    var segmentCount = 0
+    while (true) {
+      val tag: Byte = in.readByte()
+      segmentCount += 1
+
+      printf(s"#${segmentCount} Tag: %x\n", tag)
+      if (tag == 0x2c) {
+        return {}
+      } else {
+        tag match {
+          case 0xFF =>
+            val objectId = in.readLong()
+          case 0x01 =>
+            val objectId = in.readLong()
+            val refId = in.readLong()
+          case 0x02 =>
+            val objectId = in.readLong()
+            val serialNumber = in.readInt()
+            val frameNumber = in.readInt()
+          case 0x03 =>
+            val objectId = in.readLong()
+            val serialNumber = in.readInt()
+          case 0x04 =>
+            val objectId = in.readLong()
+            val serialNumber = in.readInt()
+          case 0x05 =>
+            val objectId = in.readLong()
+          case 0x06 =>
+            val objectId = in.readLong()
+            val serialNumber = in.readInt()
+          case 0x07 =>
+            val objectId = in.readLong()
+          case 0x08 =>
+            val objectId = in.readLong()
+            val threadSerialNumber = in.readInt()
+            val stackTraceSerialNumber = in.readInt()
+          case 0x20 =>
+            val objectId = in.readLong()
+            val stackTraceSerialNumber = in.readInt()
+
+            val superClassId = in.readLong()
+            val classLoaderId = in.readLong()
+            val signersId = in.readLong()
+            val domainObjectId = in.readLong()
+            val res1 = in.readLong()
+            val res2 = in.readLong()
+            val instanceSize = in.readInt()
+
+            val sizeOfPool = in.readShort()
+            for (i <- 0 until sizeOfPool) {
+              val poolIndex = in.readShort()
+
+              val bType = in.readByte()
+              val itemType = Type(bType)
+              itemType match {
+                case ObjectType =>
+                  val objId = in.readLong()
+                case BooleanType =>
+                  val value = in.readBoolean()
+                case CharType =>
+                  val value = in.readChar()
+                case FloatType =>
+                  val value = in.readFloat()
+                case DoubleType =>
+                  val value = in.readDouble()
+                case ByteType =>
+                  val value = in.readByte()
+                case ShortType =>
+                  val value = in.readShort()
+                case IntType =>
+                  val value = in.readInt()
+                case LongType =>
+                  val value = in.readLong()
+              }
+            }
+
+            val s2 = in.readShort()
+            for (i <- 0 until s2) {
+              val stringId = in.readLong()
+
+              val bType = in.readByte()
+              val itemType = Type(bType)
+              itemType match {
+                case ObjectType =>
+                  val objId = in.readLong()
+                case BooleanType =>
+                  val value = in.readBoolean()
+                case CharType =>
+                  val value = in.readChar()
+                case FloatType =>
+                  val value = in.readFloat()
+                case DoubleType =>
+                  val value = in.readDouble()
+                case ByteType =>
+                  val value = in.readByte()
+                case ShortType =>
+                  val value = in.readShort()
+                case IntType =>
+                  val value = in.readInt()
+                case LongType =>
+                  val value = in.readLong()
+              }
+            }
+
+            val s3 = in.readShort()
+            for (i <- 0 until s3) {
+              val stringId = in.readLong()
+
+              val bType = in.readByte()
+              val itemType = Type(bType)
+            }
+          case 0x21 =>
+            val objectId = in.readLong()
+            val stackTraceSerialNumber = in.readInt()
+
+            val classObjectId = in.readLong()
+            val nrOfBytes = in.readInt()
+
+            val arr = Array.ofDim[Byte](nrOfBytes)
+          case 0x22 =>
+            val objectId = in.readLong()
+            val serialNumber = in.readInt()
+
+            val nrOfElements = in.readInt()
+            val arrayId = in.readLong()
+            for (i <- 0 until nrOfElements) {
+              val arrayItem = in.readLong()
+            }
+          case 0x23 =>
+            val objectId = in.readLong()
+            val stackTraceSerialNumber = in.readInt()
+            val nrOfElements = in.readInt()
+
+            val bType = in.readByte()
+            val itemType = Type(bType)
+
+            for (i <- 0 until nrOfElements) {
+              itemType match {
+                case ObjectType =>
+                  val objId = in.readLong()
+                case BooleanType =>
+                  val value = in.readBoolean()
+                case CharType =>
+                  val value = in.readChar()
+                case FloatType =>
+                  val value = in.readFloat()
+                case DoubleType =>
+                  val value = in.readDouble()
+                case ByteType =>
+                  val value = in.readByte()
+                case ShortType =>
+                  val value = in.readShort()
+                case IntType =>
+                  val value = in.readInt()
+                case LongType =>
+                  val value = in.readLong()
+              }
+            }
+        }
+      }
+    }
   }
 }
 
@@ -28,7 +222,14 @@ sealed trait HeapDumpRecord {
   def length: Int
 }
 
-case class StringRecord(tag: Byte, length: Int) extends HeapDumpRecord
+case class Record(tag: Byte, length: Int) extends HeapDumpRecord
+case class StringRecord(id: Long, content: String, length: Int) extends HeapDumpRecord {
+  override def tag: Byte = 0x01
+}
+
+case class HeapDumpSegmentRecord(id: Int, content: String, length: Int) extends HeapDumpRecord {
+  override def tag: Byte = 0x12
+}
 
 object HeapDump {
   def apply(file: File): Option[HeapDump] = {
@@ -63,5 +264,33 @@ object HeapDump {
 
     val format = new String(arrayBuffer.toArray, "UTF8")
     format
+  }
+}
+
+sealed trait Type
+object ObjectType extends Type
+object BooleanType extends Type
+object CharType extends Type
+object FloatType extends Type
+object DoubleType extends Type
+object ByteType extends Type
+object ShortType extends Type
+object IntType extends Type
+object LongType extends Type
+
+
+object Type {
+  def apply(bType: Byte): Type = {
+    bType match {
+      case 2 => ObjectType
+      case 4 => BooleanType
+      case 5 => CharType
+      case 6 => FloatType
+      case 7 => DoubleType
+      case 8 => ByteType
+      case 9 => ShortType
+      case 10 => IntType
+      case 11 => LongType
+    }
   }
 }
--- src/main/scala/test/HeapDumpTest.scala
+++ src/main/scala/test/HeapDumpTest.scala
@@ -2,8 +2,9 @@ package test
 
 import java.io.File
 
-import heap.{HeapDump, StringRecord}
+import heap.{HeapDump, Record, StringRecord}
 
+import scala.collection.mutable.Map
 /**
   * Created by mehmetgunturkun on 22/01/17.
   */
@@ -12,14 +13,28 @@ object HeapDumpTest {
     val file = new File("/tmp/test2.dump")
     val maybeHeapDump: Option[HeapDump] = HeapDump(file)
 
+    val tagToOccurence = Map.empty[Byte, Int]
+
     maybeHeapDump match {
       case Some(heapDump) =>
-        val record = heapDump.nextRecord()
-        record match {
-          case strRec@StringRecord(tag, length) => println(strRec)
+        while (heapDump.hasNext()) {
+          val record = heapDump.nextRecord()
+          record match {
+            case StringRecord(id, content, length) =>
+            case record if record.tag == 0x0c =>
+              println(record)
+              return 0
+            case _ =>
+              val occurence = tagToOccurence.getOrElse(record.tag, 0)
+              tagToOccurence.put(record.tag, occurence + 1)
+          }
         }
       case None =>
         println("There is no heap dump to parse")
     }
+
+    tagToOccurence.foreach(println)
+
   }
 }
+
