67d8af6528fedd082dac0950ce3d031e48b1f336
mehmetgunturkun <mehmetcemgunturkun@gmail.com>
2017-02-25 16:09:30 +0300

    Refactored HeapDumpStream to use byte array for streaming too

--- src/main/scala/heap/core/HeapDumpStream.scala
+++ src/main/scala/heap/core/HeapDumpStream.scala
@@ -1,6 +1,7 @@
 package heap.core
 
 import java.io.{DataInputStream, File, FileInputStream}
+import java.nio.ByteBuffer
 
 import org.joda.time.DateTime
 
@@ -11,8 +12,26 @@ import scala.util.control.NonFatal
   * Created by mehmetgunturkun on 12/02/17.
   */
 
-class HeapDumpStream(val idSize: Int,
-                     private val data: DataInputStream) {
+
+trait HeapDumpStream {
+  def idSize: Int
+  def hasNext: Boolean
+  def read(): Byte
+  def readBoolean(): Boolean
+  def readChar(): Char
+  def readShort(): Short
+  def readInt(): Int
+  def readFloat(): Float
+  def readDouble(): Double
+  def readLong(): Long
+  def readId(): Long
+
+  def read(length: Int): Array[Byte]
+  def consume(length: Int): Unit
+}
+
+class HeapDumpStreamFromDataStream(val idSize: Int,
+                                   private val data: DataInputStream) extends HeapDumpStream {
   def hasNext: Boolean = data.available() > 0
   def read(): Byte = data.readByte()
   def readBoolean(): Boolean = data.readByte() != 0
@@ -43,6 +62,38 @@ class HeapDumpStream(val idSize: Int,
   }
 }
 
+class HeapDumpStreamFromDByteBuffer(val idSize: Int,
+                                    private val data: ByteBuffer) extends HeapDumpStream {
+  def hasNext: Boolean = data.hasRemaining
+  def read(): Byte = data.get()
+  def readBoolean(): Boolean = data.get() != 0
+  def readChar(): Char = data.getChar()
+  def readShort(): Short = data.getShort()
+  def readInt(): Int = data.getInt()
+  def readFloat(): Float = data.getFloat()
+  def readDouble(): Double = data.getDouble()
+  def readLong(): Long = data.getLong()
+  def readId(): Long = if (idSize == 4) data.getInt().toLong else data.getLong()
+
+  def read(length: Int): Array[Byte] = {
+    val arr: Array[Byte] = Array.ofDim[Byte](length)
+    data.get(arr)
+    arr
+  }
+
+  def consume(length: Int): Unit = {
+    val nrOfSlots: Int = length / 1000
+    val remainingBytes = length - (nrOfSlots * 1000)
+
+    val arr: Array[Byte] = Array.ofDim[Byte](1000)
+    for (i <- 0 until nrOfSlots) {
+      data.get(arr)
+    }
+
+    data.get(arr, 0, remainingBytes)
+  }
+}
+
 object HeapDumpStream {
   def fromFile(file: File): Option[HeapDumpStream] = {
     try {
@@ -56,13 +107,17 @@ object HeapDumpStream {
       val startTime: Long = dataInputStream.readLong()
       val startDate: DateTime = new DateTime(startTime)
 
-      val heapDumpStream = new HeapDumpStream(idSize = sizeOfId, data = dataInputStream)
+      val heapDumpStream = new HeapDumpStreamFromDataStream(idSize = sizeOfId, data = dataInputStream)
       Some(heapDumpStream)
     } catch {
       case NonFatal(e) =>
         println("Failed to parse heap dump")
         None
     }
+  }
+
+  def fromByteBuffer(idSize: Int, byteArray: Array[Byte]): HeapDumpStream = {
+    new HeapDumpStreamFromDByteBuffer(idSize, ByteBuffer.wrap(byteArray))
   }
 
   private def readFormat(dataInputStream: DataInputStream): String = {
